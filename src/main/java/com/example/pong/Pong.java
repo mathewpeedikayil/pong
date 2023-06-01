7997be8728012bc121f42fe91b15a9c954a3c8bf
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-06-01 12:30:20 +0100

    Updated the run function for the ball position, speed, direction and points system

--- src/main/java/com/example/pong/Pong.java
+++ src/main/java/com/example/pong/Pong.java
@@ -75,6 +75,36 @@ public class Pong extends Application {
             gc.setStroke(Color.WHITE);
             gc.setTextAlign(TextAlignment.CENTER);
             gc.strokeText("on Click", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+
+            // ball start position reset to middle when ball goes out of window
+            ballXPosition = WINDOW_WIDTH / 2;
+            ballYPosition = WINDOW_HEIGHT / 2;
+
+            // ball speed and direction reset
+            // the speed of the ball increases during the game
+            // the speed of the ball is slow at the beginning
+            xBallSpeed = new Random().nextInt(2) == 0 ? 1 : -1;
+            yBallSpeed = new Random().nextInt(2) == 0 ? 1 : -1;
+        }
+
+        // make sure the ball stays in the canvas
+        if(ballYPosition > WINDOW_HEIGHT || ballYPosition < 0) {
+            yBallSpeed *= -1;
+        }
+
+        // points system
+        // player two (computer) scores a point
+        // if player one (user) misses the ball
+        if(ballXPosition < playerOneXPosition - PLAYER_WIDTH) {
+            playerTwoScore++;
+            startedGame = false;
+        }
+
+        // player one (user) scores a point
+        // if player two (computer) misses the ball
+        if(ballXPosition > playerTwoXPosition + PLAYER_WIDTH) {
+            playerOneScore++;
+            startedGame = false;
         }
     }
 }
