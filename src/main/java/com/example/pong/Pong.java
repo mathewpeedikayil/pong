b90eb852f5e6398ee81e39dd7eb0a799b73904e0
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-06-01 10:58:09 +0100

    Created pong class

--- src/main/java/com/example/pong/Pong.java
+++ src/main/java/com/example/pong/Pong.java
@@ -1,18 +1,7 @@
 package com.example.pong;
 
-import java.util.Random;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class Pong extends Application {
 
